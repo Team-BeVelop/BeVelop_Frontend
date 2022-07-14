1fb16985d92258c93d8ae2b8bc91826f29e9db69
naerim <naerim1119@gmail.com>
2022-07-14 12:57:56 +0900

    [FEAT] 지원하기 template 생성

--- src/components/Recruit/ApplySection.tsx
+++ src/components/Recruit/ApplySection.tsx
@@ -1,12 +1,34 @@
 import styled from "styled-components";
 import ApplyFilter from "./ApplyFilter";
+import ApplyTemplate from "./ApplyTemplate";
 
 const ApplySection = () => {
   return (
     <>
       <ApplyFilter />
+      <Wrap>
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+        <ApplyTemplate />
+      </Wrap>
     </>
   );
 };
+
+const Wrap = styled.section`
+  overflow: hidden;
+  @media screen and (max-width: 480px) {
+    margin: 0 20px;
+  }
+`;
 
 export default ApplySection;
