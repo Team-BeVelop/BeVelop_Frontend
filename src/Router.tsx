3f4cd7c816d3c2584c7b89e527fb77370fe9e98d
SeungWon0921 <hyumapr1@gmail.com>
2023-01-31 18:54:59 +0900

    리덕스 툴킷 셋팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,37 +1,19 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { applyMiddleware, createStore } from "redux";
-import rootReducer from "./useRedux/rootReducer";
-import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
-import ReduxThunk from "redux-thunk";
 import MainPage from "./pages/MainPage";
 import UserPage from "./pages/UserPage";
 import RecruitPage from "./pages/RecruitPage";
-
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-        applyMiddleware(ReduxThunk.withExtraArgument({}), logger)
-    )
-);
-
-const persistor = persistStore(store);
+import store from "./useRedux/rootReducer";
 
 function Router() {
     return (
         <BrowserRouter>
             <Provider store={store}>
-                <PersistGate persistor={persistor}>
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/user" element={<UserPage />} />
-                        <Route path="/recruit" element={<RecruitPage />} />
-                    </Routes>
-                </PersistGate>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/user" element={<UserPage />} />
+                    <Route path="/recruit" element={<RecruitPage />} />
+                </Routes>
             </Provider>
         </BrowserRouter>
     );
